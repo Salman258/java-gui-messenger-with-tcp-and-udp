aec62c0de99c76378da650511f2df30087d0fe7d
Muhammmad Salman <33651028+Salman258@users.noreply.github.com>
2018-01-27 08:43:36 +0800

    Create Agent.java

---
+++ Agent.java
@@ -0,0 +1,24 @@
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+public class Agent {
+
+	public Socket socket;
+	
+	Agent(Socket sock){
+		this.socket = sock;
+		pingAgent();
+	}
+	
+	private void pingAgent(){
+		DataOutputStream output=null;
+		try {
+			output = new DataOutputStream(socket.getOutputStream());
+			output.writeUTF("ping ping");
+			output.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
